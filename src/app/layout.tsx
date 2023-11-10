72708d9fc3a00b4a48556839eb34096c6e2cb43d
Ege Yıldırım <egeyildirimdevelopment@gmail.com>
2023-11-10 20:39:34 +0100

    Chore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,29 +22,29 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={josefinSans.className}>
-      <body
-        className={
-          "sm:dark:bg-desktop-dark transition-all sm:bg-desktop-light dark:bg-mobile-dark bg-mobile-light bg-no-repeat bg-contain overflow-y-hidden bg-lmVeryLightGray text-lmVeryLightGray dark:bg-dmVeryDarkBlue dark:text-dmLightGrayishBlue"
-        }
-      >
-        <ThemeProvider>
-          <main
-            className={
-              "py-10 sm:py-20 px-10 sm:px-0 mx-auto w-full sm:w-4/5 md:w-3/5 xl:w-5/12 h-screen overflow-y-hidden flex flex-col"
-            }
-          >
-            <Header />
-            {children}
-            <MobileTabs />
-          </main>
-        </ThemeProvider>
-      </body>
+    <html lang="en">
+    <body
+      className={
+        "sm:dark:bg-desktop-dark transition-all sm:bg-desktop-light dark:bg-mobile-dark bg-mobile-light bg-no-repeat bg-contain overflow-y-hidden bg-lmVeryLightGray text-lmVeryLightGray dark:bg-dmVeryDarkBlue dark:text-dmLightGrayishBlue"
+      }
+    >
+    <ThemeProvider>
+    <main
+      className={
+        "py-10 sm:py-20 px-10 sm:px-0 mx-auto w-full sm:w-4/5 md:w-3/5 xl:w-5/12 h-screen overflow-y-hidden flex flex-col"
+      }
+    >
+      <Header/>
+      {children}
+      <MobileTabs/>
+    </main>
+    </ThemeProvider>
+    </body>
     </html>
   );
 }
